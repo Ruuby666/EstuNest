7e7ea759a6326ac78d8668a72f9a7db927ccb7fa
pedroporteros <pporquin@gmail.com>
2024-03-05 16:27:57 +0000

    creacion del header

---
+++ resources/views/mainPage.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="<?php echo asset('css/styles.css')?>" type="text/css"> 
+    <title>EstuNest</title>
+</head>
+<body>
+    <div class="container">
+        <div class="header">
+            <div id="logo"><img src="/img/logo.png" alt="logo EstuNest"></div>
+            <div id="menu">
+                <span>Home</span>
+                <span>Sobre Nosotros</span>
+                <span>Register</span>
+                <span>Login</span>
+            </div>
+                
+        </div>
+    </div>
+</body>
+</html>
